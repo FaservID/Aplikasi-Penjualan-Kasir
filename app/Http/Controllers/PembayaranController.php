fdd2e2883c4957f87e316763335b698b1059e376
FaservID <inggihrembang@gmail.com>
2023-05-20 15:42:32 +0700

    Pembayaran

---
+++ app/Http/Controllers/PembayaranController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreatePembayaranRequest;
+use App\Models\Pembayaran;
+use App\Models\Pesanan;
+use Illuminate\Http\Request;
+
+class PembayaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreatePembayaranRequest $request)
+    {
+        // dd($request->type);
+        if ($request->file('bukti_tf') != null) {
+            $file = $request->file('bukti_tf');
+            $extension = $file->getClientOriginalExtension();
+            // if (!in_array($file, ['png', 'jpg', 'jpeg', 'pdf'])) continue;
+            $filename = time() . '.' . $extension;
+            $filename = str_replace(' ', '-', $filename);
+            $file->move('bukti_tf', $filename);
+        }
+        $cash = [
+            'pesanan_id' => $request->pesanan_id,
+            'user_id' => $request->user_id,
+            'type' => $request->type,
+            'nominal' => $request->total_bayar
+        ];
+        $transfer = [
+            'pesanan_id' => $request->pesanan_id,
+            'user_id' => $request->user_id,
+            'type' => $request->type,
+            'bank_type' => $request->type_bank,
+            'no_rekening' => $request->no_rek,
+            'nama_rekening' => $request->nama_rek,
+            'nominal' => $request->jumlah,
+            'bukti_pembayaran' => $filename,
+
+        ];
+
+        if ($request->type == 'Cash') {
+            Pembayaran::create($cash);
+        } else {
+            Pembayaran::create($transfer);
+        }
+
+        Pesanan::where('id', $request->pesanan_id)->update(['status' => 'Pesanan diproses']);
+        return redirect()->route('pesanan.index')->with('message', 'Pembayaran Berhasil');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pembayaran $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pembayaran $pembayaran)
+    {
+        //
+    }
+}
